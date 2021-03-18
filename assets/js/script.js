518e1012ee99d10766814f37cf1162f3b1647f59
usama1s <usama_1s@hotmail.com>
2021-03-18 20:54:09 +0500

    index page under progress

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// function carouselSet(){
+  var carouselListItem = document.querySelectorAll(".carousel-list .list .carousel-item-m");
+  var l = (carouselListItem.length);
+  var w = carouselListItem[0].offsetWidth ;
+  let style = getComputedStyle(carouselListItem[0]);
+
+  var p = parseInt(style.marginLeft) + parseInt(style.marginRight);
+  console.log("width: " + w + "padding: " + p);
+
+  var ww = [(l*w) + (l*p*1.13) + "px"];
+
+  document.querySelector(".carousel-list .list").style.width = ww;
+  // document.querySelector(".carousel-list .list").style.background = "red";
+// }
+
+
+// alert("fdsf");
